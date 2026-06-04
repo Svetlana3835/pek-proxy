473f12ca434b84547fbc1e2e3baa43d5c048e3b8
Svetlana3835 <svetzakh@gmail.com>
2026-06-04 16:21:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const https = require('https');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 const server = http.createServer((req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
